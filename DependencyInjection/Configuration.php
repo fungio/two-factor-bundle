db9cbd9bd072da3d6a221d6c5cc7591b92ff7275
Pierrick Aubin <pierrick.aubin@siqual.fr>
2019-09-24 13:30:22 +0200

    Fix sf 4.3 root method deprecated in TreeBuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('fungio_two_factor');
+        $treeBuilder = new TreeBuilder('fungio_two_factor');
+        $rootNode    = $treeBuilder->getRootNode();
 
         $supportedDrivers = ['orm', 'custom'];
 
